fad30ca8e144287c7ab80daed6ead6870b1f2a08
Prakashrcc <38122792+Prakashrcc@users.noreply.github.com>
2022-09-10 12:48:15 +0530

    Add files via upload
    fs

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ActivityComponent } from './activity/activity.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomePageComponent,
+    HeaderComponent,
+    FooterComponent,
+    ActivityComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,122 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import placesList from '../places.json';  
+import {ActivityServiceService} from '../services/activity-service.service';
+import { AutoSuggestService } from '../services/auto-suggest.service';
+import {Router} from '@angular/router';
+
+interface Place {  
+  id: String;  
+  name: String;  
+  state: String;  
+}  
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+export class HomePageComponent implements OnInit {
+  mouseInSearch = false;
+  searchComponent;
+  places: Place[] = placesList;  
+  tempPlaces: Place[] = placesList;
+  selectedPlace: Place = null;
+  activityStartDate:String ;
+  activityEndDate:String ;
+  public autoSuggestData :any[] = [] ;
+
+  constructor(private http:HttpClient, private service:ActivityServiceService, private route:Router,
+    public autosearchService: AutoSuggestService) { 
+    console.log(JSON.stringify(this.tempPlaces));
+    this.selectedPlace= {
+      id : '', name : '',state:'',
+    }
+    this.selectedPlace.name = 'Going to';
+  }
+
+  ngOnInit(): void {
+  }
+
+  navigateToActivity() {
+    console.log('in navigate'+this.activityStartDate);
+    
+    this.route.navigate(['/activity'],{ queryParams: {lat: 37,long:50, from: this.activityStartDate, to : this.activityEndDate}});
+  }
+
+  getAutosuggest(event){
+    var st = event.target.value;
+    console.log(st)
+    this.autosearchService.autoSuggest(st).subscribe((response:any) => {
+    console.log("res->",response);
+    this.autoSuggestData = response.s;
+    console.log("autoSuggestData->",this.autoSuggestData);
+  })
+  
+ }
+
+  openTab(evt,tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+
+  openSearch(event,evtId) {
+    this.searchComponent = document.getElementById(evtId);
+    this.searchComponent.style.display = 'block';
+    document.getElementById('searchInput').focus();
+    document.addEventListener('mousedown', (e) => {
+      if (!this.mouseInSearch) {
+        this.searchComponent.style.display = 'none';
+        document.removeEventListener("mousedown", (e) => {});
+      }
+    })
+  }
+
+  mouseOutHandler() {
+    this.mouseInSearch = false;
+  }
+
+  mouseInHandler() {
+    this.mouseInSearch = true;
+  }
+
+  onSearchInputChange(event) {
+    var term = event.target.value;
+    var lowerTerm = term.toLowerCase();
+    console.warn(lowerTerm );
+    this.tempPlaces = [];
+    for (var index = 0; index < this.places.length; index++) {
+      if (((this.places[index].name).toLowerCase()).startsWith(lowerTerm)) {
+        this.tempPlaces.push(this.places[index]);
+      }
+    }
+  }
+
+  selectPlace(event,place) {
+    console.log('i m here');
+    this.selectedPlace = place;
+    this.searchComponent.style.display = 'none';
+    console.log(this.selectedPlace);
+  }
+
+  selectDate(event) {
+    console.warn('i m');
+    var ele = event.target;
+    ele.daterangepicker({
+      "startDate": "09/02/2022",
+      "endDate": "09/08/2022"
+  }, function(start, end, label) {
+    console.log("New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')");
+  });
+  }
+
+}
